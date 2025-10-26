8aa7ba8f6b61ce07cf67c634cdf1d46d836be75a
fauzanashshidiq <muhzeen07@gmail.com>
2025-10-27 01:48:07 +0700

    feat(first-activity): add first page for welcome greeting

---
+++ app/src/main/java/com/example/dapurtongseng/FirstActivity.kt
@@ -0,0 +1,32 @@
+package com.example.dapurtongseng
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dapurtongseng.databinding.ActivityFirstBinding
+
+class FirstActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnStartNow.setOnClickListener {
+             val intent = Intent(this, MainActivity::class.java)
+             startActivity(intent)
+        }
+        supportActionBar?.hide()
+    }
+}
