300747544eb7ae20db589b12a515dd628fff71dd
fauzanashshidiq <muhzeen07@gmail.com>
2025-10-29 02:18:52 +0700

    feat(profile-fragment): add username with get string FULL_NAME with SharedViewModel

--- app/src/main/java/com/example/dapurtongseng/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dapurtongseng/ui/profile/ProfileFragment.kt
@@ -4,37 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import com.example.dapurtongseng.SharedViewModel
 import com.example.dapurtongseng.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-private var _binding: FragmentProfileBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
-    _binding = FragmentProfileBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textProfile
-    profileViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedViewModel.fullName.observe(viewLifecycleOwner) { name ->
+            binding.textProfile.text = if (!name.isNullOrEmpty()) "Nama Pengguna: $name" else "Nama Pengguna: Pelanggan"
+        }
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
