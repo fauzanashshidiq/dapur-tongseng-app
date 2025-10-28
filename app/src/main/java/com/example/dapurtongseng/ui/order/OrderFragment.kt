48dbf10d3d889e27dd69ddb712c4c8c9309c2463
fauzanashshidiq <muhzeen07@gmail.com>
2025-10-29 00:59:48 +0700

    feat: change name file of ui bottom navigations

--- app/src/main/java/com/example/dapurtongseng/ui/order/OrderFragment.kt
+++ app/src/main/java/com/example/dapurtongseng/ui/order/OrderFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dapurtongseng.ui.dashboard
+package com.example.dapurtongseng.ui.order
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.dapurtongseng.databinding.FragmentDashboardBinding
+import com.example.dapurtongseng.databinding.FragmentOrderBinding
+import com.example.dapurtongseng.ui.dashboard.OrderViewModel
 
-class DashboardFragment : Fragment() {
+class OrderFragment : Fragment() {
 
-private var _binding: FragmentDashboardBinding? = null
+private var _binding: FragmentOrderBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -21,14 +22,14 @@ private var _binding: FragmentDashboardBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    val orderViewModel =
+            ViewModelProvider(this).get(OrderViewModel::class.java)
 
-    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    _binding = FragmentOrderBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textDashboard
-    dashboardViewModel.text.observe(viewLifecycleOwner) {
+    val textView: TextView = binding.textOrder
+    orderViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
     return root
